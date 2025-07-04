1ef996aec8fda24ee5abfe8b97445ef0f01402ca
jupiter-hongc <167389344+jupiter-hongc@users.noreply.github.com>
2025-07-04 12:02:04 +0800

    docs: update docs

---
+++ examples/basic/lws.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  rewrite: [
+    {
+      from: "/bapi/(.*)",
+      to: "https://www.asterdex.com/bapi/$1",
+    },
+    {
+      from: "/fapi/(.*)",
+      to: "https://www.asterdex.com/fapi/$1",
+    },
+    {
+      from: "/cloud-futures/(.*)",
+      to: "https://static.asterdex.com/cloud-futures/$1",
+    },
+    {
+      from: "/api/(.*)",
+      to: "https://static.asterdex.com/api/$1",
+    },
+    {
+      from: "/(.*)/futures/(.*)",
+      to: "/index.html",
+    },
+  ],
+  spa: "index.html",
+  directory: "./",
+  logFormat: "stats",
+};
